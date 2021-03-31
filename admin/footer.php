ec33ef12446821eb140184b313df2b9f85b196b7
yassineYAYAbouzar <76868426+yassineYAYAbouzar@users.noreply.github.com>
2021-03-31 15:29:00 +0100

    Delete admin directory

--- admin/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="text-center">
-    <i class="text-white">Created &copy; By Yassine Bouzar 2021<i>
-</footer>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-    </body>
-</html>
